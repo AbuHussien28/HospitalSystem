65ddc2445f86a51bb8ab166556705952c360872e
Aya-Elzoghby21 <ayaelzoghby651@gmail.com>
2025-03-15 00:07:53 +0200

    Add Reports

---
+++ ProjectHospitalSystem/Reports/Reports&Logging.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ProjectHospitalSystem.Reports
+{
+    partial class Reports_Logging
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_RepNumberPatient = new Button();
+            btn_RepSechedule = new Button();
+            btn_RepOutstandPay = new Button();
+            btn_RepVistHist = new Button();
+            SuspendLayout();
+            // 
+            // btn_RepNumberPatient
+            // 
+            btn_RepNumberPatient.Location = new Point(12, 56);
+            btn_RepNumberPatient.Name = "btn_RepNumberPatient";
+            btn_RepNumberPatient.Size = new Size(253, 81);
+            btn_RepNumberPatient.TabIndex = 0;
+            btn_RepNumberPatient.Text = "Number of patients per department";
+            btn_RepNumberPatient.UseVisualStyleBackColor = true;
+            btn_RepNumberPatient.Click += btn_RepNumberPatient_Click;
+            // 
+            // btn_RepSechedule
+            // 
+            btn_RepSechedule.Location = new Point(326, 56);
+            btn_RepSechedule.Name = "btn_RepSechedule";
+            btn_RepSechedule.Size = new Size(251, 81);
+            btn_RepSechedule.TabIndex = 1;
+            btn_RepSechedule.Text = "Appointment schedules ";
+            btn_RepSechedule.UseVisualStyleBackColor = true;
+            btn_RepSechedule.Click += btn_RepSechedule_Click;
+            // 
+            // btn_RepOutstandPay
+            // 
+            btn_RepOutstandPay.Location = new Point(12, 172);
+            btn_RepOutstandPay.Name = "btn_RepOutstandPay";
+            btn_RepOutstandPay.Size = new Size(253, 87);
+            btn_RepOutstandPay.TabIndex = 2;
+            btn_RepOutstandPay.Text = "Outstanding payments ";
+            btn_RepOutstandPay.UseVisualStyleBackColor = true;
+            btn_RepOutstandPay.Click += btn_RepOutstandPay_Click;
+            // 
+            // btn_RepVistHist
+            // 
+            btn_RepVistHist.Location = new Point(326, 172);
+            btn_RepVistHist.Name = "btn_RepVistHist";
+            btn_RepVistHist.Size = new Size(251, 87);
+            btn_RepVistHist.TabIndex = 3;
+            btn_RepVistHist.Text = "Patient visit history ";
+            btn_RepVistHist.UseVisualStyleBackColor = true;
+            btn_RepVistHist.Click += btn_RepVistHist_Click;
+            // 
+            // Reports_Logging
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(630, 373);
+            Controls.Add(btn_RepVistHist);
+            Controls.Add(btn_RepOutstandPay);
+            Controls.Add(btn_RepSechedule);
+            Controls.Add(btn_RepNumberPatient);
+            Name = "Reports_Logging";
+            Text = "Reports_Logging";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn_RepNumberPatient;
+        private Button btn_RepSechedule;
+        private Button btn_RepOutstandPay;
+        private Button btn_RepVistHist;
+    }
+}
---
+++ ProjectHospitalSystem/Reports/Reports&Logging.cs
@@ -0,0 +1,58 @@
+﻿using ProjectHospitalSystem.Forms.Receptionist;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjectHospitalSystem.Reports;
+using DevExpress.XtraReports.UI;
+using ProjectHospitalSystem.Models;
+
+namespace ProjectHospitalSystem.Reports
+{
+    public partial class Reports_Logging : Form
+    {
+        private readonly HospitalSystemContext _context = new HospitalSystemContext();
+        public Reports_Logging()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_RepNumberPatient_Click(object sender, EventArgs e)
+        {
+            Report1 report = new Report1(); 
+            ReportPrintTool printTool = new ReportPrintTool(report);
+            printTool.ShowPreviewDialog();
+        }
+
+        private void btn_RepSechedule_Click(object sender, EventArgs e)
+        {
+
+            Report2 report = new Report2();
+            ReportPrintTool printTool = new ReportPrintTool(report);
+            printTool.ShowPreviewDialog();
+
+        }
+
+        private void btn_RepOutstandPay_Click(object sender, EventArgs e)
+        {
+
+            Report3 report = new Report3();
+            ReportPrintTool printTool = new ReportPrintTool(report);
+            printTool.ShowPreviewDialog();
+
+        }
+
+        private void btn_RepVistHist_Click(object sender, EventArgs e)
+        {
+
+            Report4 report = new Report4();
+            ReportPrintTool printTool = new ReportPrintTool(report);
+            printTool.ShowPreviewDialog();
+        }
+    }
+}
